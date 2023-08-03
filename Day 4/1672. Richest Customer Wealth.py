e3af93b763cc6ad784236983346a0cbe6ebbf23c
Night-Owl-18 <126809431+Night-Owl-18@users.noreply.github.com>
2023-08-03 19:49:51 +0530

    Create 1672. Richest Customer Wealth.py

---
+++ Day 4/1672. Richest Customer Wealth.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumWealth(self, a: List[List[int]]) -> int:
+        c=[]
+        for i in range(len(a)):
+            t=0
+            for j in range(len(a[i])):
+                t+=a[i][j]
+            c.append(t)
+        return max(c)
+
+    """
+    return max([sum(i) for i in a])
+
+    """
